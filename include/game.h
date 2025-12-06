a19b3e239eb806a83079590cbe3b82ac9bfa36d8
Hjalmar Albinsson <51383404+Hjalmar-Albinsson@users.noreply.github.com>
2025-12-06 14:08:34 +0100

    Clean up game.h by removing comments and functions
    Removed comments and unnecessary function declarations.

--- include/game.h
+++ include/game.h
@@ -1,20 +1,5 @@
 #ifndef GAME_H
 #define GAME_H
-
-/* Core game public API (minimal) */
-
-#include <stdbool.h>
-
-/* Initialize the game world */
 void game_init(void);
-
-/* Run one tick of the main loop */
-void game_tick(void);
-
-/* Query if player has won */
-bool game_won(void);
-
-/* Run the full blocking game loop (host-mode) */
 void game_loop(void);
-
-#endif /* GAME_H */
+#endif
